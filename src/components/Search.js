76f6266a23b83b20116c0e7bb0b65431a0b3a53a
Brian Yoon <infobyoon@gmail.com>
2022-04-11 12:37:08 -0700

    attempt to make search feature

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { useState } from 'react';
 
 function Search({ searchBar, setSearchBar, handleSearch }) {
+    const handleChange = (event) => {
+        setSearchBar ({...searchBar, wallet: event.target.value})
+    }
 	return (
 		<div>
-			<form action=''></form>
+			<form id='formContainer' onSubmit={handleSearch}>
+				<label htmlFor='menuEl'> Search Bar</label>
+				<input
+					type='text'
+					id='menuEl'
+					onChange={handleChange}
+					value={searchBar.menuEl}
+				/>
+				<button type='submit'>Submit</button>
+			</form>
 		</div>
 	);
 }
