86868f93df39096789812563096811a08839ce46
Brian Yoon <infobyoon@gmail.com>
2022-04-13 12:49:28 -0700

    working carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+// import React, { Component } from 'react';
+// import axios from 'axios';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
 	const [searchBar, setSearchBar] = useState();
-  // attempt to display image to gallery
-  const [image, setImage] = useState();
-  // storing response to nft data
-  const [nftData, setNftData] = useState();
+	// attempt to display image to gallery
+	const [image, setImage] = useState();
+	// storing response to nft data
+	const [data, setData] = useState();
+	const [galleryItems, setGalleryItems] = useState([]);
 
 	function handleSearch(event) {
 		event.preventDefault();
@@ -21,12 +26,59 @@ function App() {
 			.then((response) => response.json())
 			// .then((response) => console.log(response))
 			.then((response) => {
-        console.log(response.tokens[0])
-        setImage(response.tokens[0].imageUrl)
-      })
+				// console.log(response.tokens[0])
+				console.log(response.tokens);
+				const data = response.tokens;
+				const image = data.filter((e) => e.imageUrl !== null).map((m) => <img src={m.imageUrl} alt='' />);
+				setGalleryItems(image);
+				// setImage(response.tokens[0].imageUrl)
+			})
 			.catch((err) => console.error(err));
 	}
+	const responsive = {
+		0: { items: 1 },
+		1024: { items: 2 },
+	};
+	// class App extends Component {
+	//   	const [searchBar, setSearchBar] = useState();
+	// 	  // attempt to display image to gallery
+	// 	  const [image, setImage] = useState();
+	// 	  // storing response to nft data
+	// 	  const [data, setData] = useState();
 
+	// 	constructor(props) {
+	// 		super(props);
+	// 		this.state = {
+	// 			galleryItems: [],
+	// 		};
+	// 	}
+
+	// 	getData() {
+	// 		axios
+	// 			.get(
+	// 				`https://api.modulenft.xyz/api/v1/opensea/user/tokens?user=${searchBar.wallet}`,
+	// 				{}
+	// 			)
+	// 			.then((res) => {
+	// 				const data = res.data;
+	// 				const img = data.map((m) => <img src={m.download_url} alt='' />);
+	// 				this.setState({
+	// 					galleryItems: img,
+	// 				});
+	// 			})
+	// 			.catch((error) => {
+	// 				console.log(error);
+	// 			});
+	// 	}
+	// 	responsive = {
+	// 		0: { items: 1 },
+	// 		1024: { items: 2 },
+	// 	};
+	// 	componentDidMount() {
+	// 		this.getData();
+	// 	}
+
+	// render() {
 	return (
 		<div className='app-container'>
 			<nav>
@@ -43,13 +95,32 @@ function App() {
 					<Route
 						path='/'
 						element={
-							<Home searchBar={searchBar} setSearchBar={setSearchBar} handleSearch={handleSearch} />
+							<Home
+								searchBar={searchBar}
+								setSearchBar={setSearchBar}
+								handleSearch={handleSearch}
+							/>
 						}></Route>
 				</Routes>
 			</main>
-      <img src={image} alt='image'></img>
+			{/* <img src={image} alt='image'></img> */}
+			<div>
+				{galleryItems.length > 0 && (
+					<AliceCarousel
+						items={galleryItems}
+						responsive={responsive}
+						autoPlayInterval={2000}
+						autoPlayDirection='rtl'
+						autoPlay={true}
+						fadeOutAnimation={true}
+						mouseTrackingEnabled={true}
+						disableAutoPlayOnAction={true}
+					/>
+				)}
+			</div>
 		</div>
 	);
 }
+// }
 
 export default App;
